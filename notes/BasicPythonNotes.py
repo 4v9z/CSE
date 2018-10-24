e1523108451239124f18191dab17f85d19caca63
4v9z <42391837+4v9z@users.noreply.github.com>
2018-10-24 14:39:05 -0700

    Added more basic notes

---
+++ notes/BasicPythonNotes.py
@@ -0,0 +1,48 @@
+print ("Hello World")
+
+# Apparently I'm going to slow, so I will speed up' \
+# 'This is a comment
+# This has no effect on the code
+# This is used for a variety of things, such as
+# 1. Making personal notes about my code
+# 2. Commenting out code that does not work
+
+print("Notice what is happening here")
+print()  # This creates a new line
+print()  # Do you notice the underline here?
+# Hover over top it to see what is wrong.
+
+# Math
+print(5+3)
+print(5-3)
+print(4*5)
+print(6/5)
+
+
+# Semi-advanced math
+print("Figure this out...")
+print(6//4)
+print(12//3)
+print(9//4) # This will only give you a whole number!
+print()
+
+print("Figure this out too...")
+print(6%4)
+print(5%3) # This finds the remainder after subtracting until subtracting once more would get you a negative number
+print(9%4)
+
+# Defining Variables
+car_name = "Wiebe mobile"  # String
+car_type = "Tesla"  # String
+car_cyllinders=16  # Integer
+car_miles_per_gallon=0.01  # Float
+
+print("I have a car callled %s; It's pretty fresh." % car_name)
+print("It has %d cyllinders, but gets %f mpg" %(car_cyllinders, car_miles_per_gallon))
+
+# Taking Input
+name = input ("What is your name?")
+print ("Hello %s!" % name)
+
+age = input ("How old are you?")
+print("%s? You belong in a museum!" % age)
