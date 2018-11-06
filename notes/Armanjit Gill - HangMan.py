f3d4eb9f3f867848e3522b584ea134a1cb953aca
4v9z <42391837+4v9z@users.noreply.github.com>
2018-11-06 14:36:30 -0800

    Continued to Work on HangMan

--- notes/Armanjit Gill - HangMan.py
+++ notes/Armanjit Gill - HangMan.py
@@ -1,5 +1,50 @@
+import random
+Won = False
+first_letter = 1
+second_letter = 2
+third_letter = 3
+fourth_letter = 4
+fifth_letter = 5
+sixth_letter = 6
+guesses_left = 8
 print("Hangman! []____0-/-<")
-Word = str("Soup")
+Words = ("dragon", "zoinks", "demons","hanger", "jumped")
+random_word = random.choice(Words)
+if random_word == "dragon":
+    first_letter = "d"
+    second_letter = "r"
+    third_letter = "a"
+    fourth_letter = "g"
+    fifth_letter = "o"
+    sixth_letter = "n"
+if random_word == "zoinks":
+    first_letter = "z"
+    second_letter = "o"
+    third_letter = "i"
+    fourth_letter = "n"
+    fifth_letter = "k"
+    sixth_letter = "s"
+if random_word == "demons":
+    first_letter = "d"
+    second_letter = "e"
+    third_letter = "m"
+    fourth_letter = "o"
+    fifth_letter = "n"
+    sixth_letter = "s"
+if random_word == "hanger":
+    first_letter = "h"
+    second_letter = "a"
+    third_letter = "n"
+    fourth_letter = "g"
+    fifth_letter = "e"
+    sixth_letter = "r"
+if random_word == "jumped":
+    first_letter = "j"
+    second_letter = "u"
+    third_letter = "m"
+    fourth_letter = "p"
+    fifth_letter = "e"
+    sixth_letter = "d"
 Man = input("What is the name of the person who will be hung? ")
 You = input("You are %s's last chance. What is your name? " % Man)
 print()
@@ -18,13 +63,17 @@ print("Time to make a choice! Respond with 1 to say yes, respond with 2 to say n
 Life_or_Death = int(input("%s: Should I save %s?" % (You, Man)))
 
 if Life_or_Death == 1:
-    print("%s: I'll do it! Why you'd allow English Homework to decide someone's life is beyond me, but I'll do it!" % You)
-    guess = input("What is the word?")
-    if guess == Word:
+    print("%s: I'll do it! Why you'd have English Homework decide someone's life is beyond me, but I'll do it!" % You)
+    while guesses_left > 0:
+        guess = input("What is the first letter?")
+     guesses_left -= 1
+    if guess == first_letter:
+         print(first_letter)
+         if Won:
         input("Executioner: Wow! You did it! Guess I'll let %s free." % Man)
         input("%s: Thank you %s! You saved me!" % (Man, You))
         input("%s: If there is anything I can do to repay your kindness all you need to do is ask!" % Man)
-        input("%s I'll let you know if something comes up" % You)
+        input("%s It was nothing, I did the good!" % You)
         input("THE END")
         print("You got the good ending! Congratulations!")
 elif Life_or_Death == 2:
