17c31e17c8a122d2a25bdec6fca54d23f7abd998
4v9z <42391837+4v9z@users.noreply.github.com>
2018-11-28 14:37:53 -0800

    Mad Libs 2: Electric Boogaloo

---
+++ notes/Armanjit Gill - Mad Libs 2.py
@@ -0,0 +1,57 @@
+# Time for some hecking Angry Libs! (poyo!)=> <(l.l<)
+print("Please no swearing and/or inappropriate words for a classroom")
+
+Words = [input("What is your name? "),
+    input("Give me a name of person"),
+    input("Adjective Time! Give me one!"),
+    input("Can you give me a verb?"),
+    input("Can you give me a second verb?"),
+    input("I'm gonna need a noun now."),
+    input("I'm gonna need another noun"),
+    input("I need a plural noun next"),
+    input("Can I get a verb?"),
+    input("I need a third noun"),
+    input("I need a new plural noun"),
+    input("I need a fourth verb now..."),
+    input("I need a new adjective"),
+    input("Give me another person name"),
+    input("I need a third adjective"),
+    input("Give me a character from a game, book or show"),
+    input("Give me another character from a game, book or show"),
+    input("It's a triple wammy! Give me another character from a game, book or show"),
+    input("Time for a new noun"),
+    input("Can I get another noun?"),
+    input("Tic-tac-toe! 3 nouns in a row"),
+    input("...I need another noun..."),
+    input("Another noun.... something needs to break this streak"),
+    input("I need an adjective now (Finally! Something that isn't a noun)"),
+    input("I need another adjective now"),
+    input("Give me a character from a game, book or show"),
+    input("New adjective! Go!"),
+    input("Give me a new character from a game, book or show"),
+    input("I need a new character now"),
+    input("Give me some kind of video game"),
+    input("Give me a number! Large or small, it doesn't matter!")]
+print()
+print("Time for the Mad Lib!")
+print()
+print()
+print()
+print("The New Super Smash Bros")
+print("Super Smash Bros is a game made by %s where %s characters %s (with) each other" % (Words[1], Words[2], Words[3]))
+print("The objective is to %s each other off the %s." % (Words[4], Words[5]))
+print("There are many modes like %s battle where you fight to make characters lose %s" % (Words[6], Words[7]))
+print("or %s battle where you %s your opponents’ %s to defeat them." % (Words[8], Words[9], Words[10]))
+print("Sometimes there is a story mode, last time you had to %s the %s %s," % (Words[11], Words[12], Words[13]))
+print(", it was the most %s way to %s characters." % (Words[14], Words[15]))
+print("In the game, you can play as characters like %s or %s." % (Words[16], Words[17]))
+print(" Sadly, they didn’t add in %s." % (Words[18]))
+print("There are lots of cool items too! There’s (a/an) %s, %s, %s" % (Words[19], Words[20], Words[21]))
+print(" and %s just to name a few." % Words[22])
+print("Of course, when characters get the %s they can use a %s." % (Words[23], Words[24]))
+print("You shouldn't choose characters who are %s or %s. " % (Words[25], Words[26]))
+print("I heard that they might add %s, %s characters, %s or %s!" % (Words[27], Words[28], Words[29], Words[30]))
+print("I sure (don’t) hope they add %s from %s!" % (Words[31], Words[32]))
+print("These games are really fun. I look forward to when they come out in %s weeks." % Words[33])
+print()
+print("You sure wrote a funny story ", Words[0], "!")
