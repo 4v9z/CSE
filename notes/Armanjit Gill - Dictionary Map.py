3a57cc2cec2675faa6b7928138068852b55beb4b
4v9z <42391837+4v9z@users.noreply.github.com>
2019-02-04 14:38:22 -0800

    tu7hfdatyjk'lf

--- notes/Armanjit Gill - Dictionary Map.py
+++ notes/Armanjit Gill - Dictionary Map.py
@@ -1,41 +1,45 @@
 world_map = {
-    'R19A': {
-        'NAME': "Mr. Wiebe's room",
-        'DESCRIPTION': "This is the room that you are in",
+    'ADVENTURE_START': {
+        'NAME': "An Adventure's Beginning",
+        'DESCRIPTION': "You stand atop a hill looking ahead at the forest "
+                       "to the north and turn around to see the desert to the south. You're ready for your quest.",
         'PATHS': {
-            'NORTH': "PARKING_LOT"
+            'NORTH': "FOREST",
+            'SOUTH': "DESERT_TOWN"
         }
     },
-    'PARKING_LOT': {
-        'NAME': "A Parking Lot",
-        'DESCRIPTION': "There are a few cars parked here",
+    'FOREST': {
+        'NAME': "Lost Woods",
+        'DESCRIPTION': "This is a strange place. You hear a faint wind that whistles: 'konami....",
         'PATHS': {
-            'SOUTH': "R19A",
-            "DOWN": "MARX_ARENA"
+            'NORTH': "FOREST_2",
+            "SOUTH": "ADVENTURE_START"
         }
     },
-    'MARX_ARENA': {
-        'NAME': "Marx's Domain",
-        'DESCRIPTION': "You find yourself somewhere that looks like a planet separate from ours, this place is "
-                       "barren, devoid of any life. This is not a safe place, the only thing here is "
-                       "Marx, ready for battle. "
-                       "Marx is staring you down, waiting "
-                       "for you to step forwards",
+    'FOREST_2': {
+        'NAME': "Lost Woods",
+        'DESCRIPTION': "You've made it to the second part of the forest, remember the hint",
         'PATHS': {
-            'UP': "PARKING_LOT",
-            'NORTH': "MARX_FIGHT"
+            'NORTH': "FOREST_3",
+            'SOUTH': "FOREST"
         }
+    },
+    'FOREST_3': {
+        "NAME": "Lost Woods",
+        'DESCRIPTION': "Third room, but where to next?"
     }
 
 }
 
 # Other variables
 directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN"]
-current_node = world_map['R19A']     # This is your current location
+current_node = world_map['ADVENTURE_START']     # This is your current location
 playing = True
 
 # Controller
 while playing:
+    if current_node['NAME'] == "Lost Woods":
+        in_lost_woods = True
     print(current_node['NAME'])
     command = input(">_")
     if command.lower() in ['q', 'quit', 'exit']:
@@ -44,7 +48,7 @@ while playing:
         print(current_node["DESCRIPTION"])
     elif command == "recognized":
         print("Command not reco- Oh... VERY funny! HA! HA! HA! Don't do that again")
-    elif command in directions:
+    elif command.upper() in directions:
         try:
             room_name = current_node["PATHS"][command]
             current_node = world_map[room_name]
