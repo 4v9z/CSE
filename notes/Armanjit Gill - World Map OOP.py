a64fc86871fe2c342e79795a8b1a43cbc7f9b1b3
4v9z <42391837+4v9z@users.noreply.github.com>
2019-02-22 14:38:20 -0800

    I want be the best there ever was To beat all the rest, yeah that's my cause Electrode, Diglett, Nidoran, Mankey Venusaur, Rattatta, Fearow, Pidgey Seaking, Jolteon, Dragonite, Gastly Ponyta, Vaporeon, Poliwrath, Butterfree Catch 'em, catch 'em, gotta catch 'em all Pokemon I'll search across the land, look far and wide Release from my hand the power that's inside Venomoth, Poliwag, Nidorino, Golduck Ivysaur, Grimer, Victreebel, Moltres Nidoking, Farfetch'd, Abra, Jigglypuff Kingler, Rhyhorn, Clefable, Wigglytuff Catch 'em, catch 'em, gotta catch 'em all Gotta catch 'em all Pokemon Zubat, Primeape, Meowth, Onix Geodude, Rapidash, Magneton, Snorlax Gengar, Tangela, Goldeen, Spearow Weezing, Seel, Gyarados, Slowbro Gotta catch…

--- notes/Armanjit Gill - World Map OOP.py
+++ notes/Armanjit Gill - World Map OOP.py
@@ -1,5 +1,5 @@
 class Room(object):
-    def __init__(self, name, description='This is a room', north=None, south=None, east=None, west=None,
+    def __init__(self, name='ROOM', description='This is a room', north=None, south=None, east=None, west=None,
                  up=None, down=None, enter=None, leave=None):
         self.name = name
         self.description = description
@@ -14,16 +14,49 @@ class Room(object):
 
 
 # Option 2 - Use strings, but more difficult controller
+VOLCANO = Room('Volcano', "You are in front of a volcano... do you want to jump in?")
+MT_SILVER = Room('Mt. Silver', "You are on top of the mountain. Hail is plummeting down and the wind is howling. "
+                               "There is someone at the edge of the cliff you're standing on"
+                               "\n Red:..............!!!", None, None, None, None, None, 'MTN_BASE')
+MTN_BASE = Room('Mt. Silver Base', "You are at the base of a snowy mountain. You can likely climb it, but the rocks "
+                                   "are slightly slippery, so you might slip and fall", 'VOLCANO', None, None, None,
+                'MT_SILVER', 'MTN_PASS')
+CAVE = Room('Frozen Cave', "You are in a cold cave. The ground and walls are frozen and "
+                           "there are icicles hanging from the ceiling"
+                           "\n In the center of the cave is an icy sword encased in an impenetrable layer of ice. "
+                           "It seems like this ice wasn't here before... If only you could rewind time...",
+            None, None, 'MTN_PASS')
+MTN_PASS = Room('Mountain Pass', "There is a frozen path leading west, a shop to the east, "
+                                 "and towering above you, a mountain",
+                None, None, 'MTN_SHOP', "CAVE", 'MTN_BASE')
+MTN_SHOP = Room('Mountain Shop', "There is engraving on stone: BUY SOMETHING AND LEAVE THE MONEY OR ELSE..."
+                                 "\n There is a keycard, armor, health upgrades, and a space helmet",
+                None, None, 'CLIMB', 'MTN_PASS')
+BAY = Room('Great Bay', 'There is an open ocean around you, you can see a strange structure under the water',
+           None, None, None, 'RIVER', None, 'TEMPLE')
 BEGIN = Room("An Adventure's Beginning", "You stand atop a hill looking ahead at the forest to "
                                          "the north and turn around to see the desert to the south. "
                                          "\n You're ready for your quest.", 'FOREST', 'TOWN', 'OASIS',
              'FACTORY', 'CHEATS', 'LOSS')
+MARKET = Room('Desert Market', "You browse the fine selection of goods, you see potions that increase health and MP,"
+                               "\n a strange pendant with a drop of water engraved on it, a scimitar, strange scuba "
+                               "gear, and an odd key "
+                               "\n You also see a battered rubber door mat saying 'WELCOME TO ZORK', but it "
+                               "seems to be worthless. "
+                               "\n You can buy or sell something here.")
+TOWN = Room('Desert Town', "You are in a barren town, there isn't much to see here, save for some bags of gold..."
+                           "\n But you wouldn't steal from innocent people... would you?",
+            'BEGIN', 'MARKET', 'DESERT_FIGHT')
+CHEATS = Room()
+OASIS = Room("Desert Oasis", "You're in the middle of a desert next to the only water here. "
+                             "\n There is a waterway barely big enough for you in the water.",
+             'RIVER', 'DESERT_FIGHT', None, 'BEGIN', None, 'TOWN')
 LOSS = Room('|     |i   ||  |__', '|     |i   ||  |__')
 FACTORY = Room('Factory', "You are looking at a strange factory, will you enter it?", None, None, 'BEGIN', None,
                'M_MARIO')
 M_MARIO = Room('Inside the Factory', "You are in a fight with Metal Mario, a Robotic "
                                      "copy of the beloved plumber! Let's see if you can win!",
-               None, None, None, None)
+               None, None, None, None, None, None, None, 'FACTORY')
 FOREST = Room("Lost Woods", 'You are in a forest that feels mysterious, if you make a wrong move,'
                             ' you will be sent back to the first room of the forest.', 'FOREST2', 'FOREST',
               'FOREST', 'FOREST')
@@ -46,5 +79,14 @@ FOREST8 = Room('Lost Woods', 'You are in a forest that feels mysterious, if you 
 CLEARING = Room('Clearing', "You've made it to a clearing, you can move East, West, or North."
                             "\n "
                             "There is also a strange sword embedded in the ground, however, "
-                            "it's dark and seems to have lost all of it's power", "MOUNTAIN_PASS",
-                "FOREST", "JEVIL_ENTRANCE", "RIVER_PATH")
+                            "it's dark and seems to have lost all of it's power", "MTN_PASS",
+                "FOREST", "JEVIL_ENTRANCE", "RIVER")
+RIVER = Room('River Path', 'There is a small river flowing next to you.'
+                           '\n You can follow it to the east or you can go North, South, or West',
+             'MTN_SHOP', 'OASIS', 'BAY', 'CLEARING')
+JEVIL_ENTRANCE = Room('???????????', "*There is a cage-like gate in front of you, "
+                                     "* There's a note saying: 'Collect the 4 keys to enter'",
+                      None, None, 'CLEARING', None, None, None, 'JEVIL_FIGHT')
+JEVIL_FIGHT = Room("???????", "JEVIL: 'I CAN DO ANYTHING!!' Watch out! Here comes JEVIL! "
+                              "There's no strategy to beat this enemy, Good Luck! LET THE GAMES BEGIN!",
+                   None, None, None, None, None, None, None, 'JEVIL_ENTRANCE')
