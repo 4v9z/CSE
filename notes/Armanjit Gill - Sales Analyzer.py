67b6e670a81cec492da9b788fdb44cb301cf80db
4v9z <42391837+4v9z@users.noreply.github.com>
2019-05-09 14:15:09 -0700

    a

--- notes/Armanjit Gill - Sales Analyzer.py
+++ notes/Armanjit Gill - Sales Analyzer.py
@@ -2,6 +2,7 @@ import csv
 
 
 def highest_profit(aa, aaa):
+<<<<<<< HEAD
     fruit_profit = 0
     cosmetic_profit = 0
     baby_profit = 0
@@ -41,6 +42,22 @@ def highest_profit(aa, aaa):
     the_profits = [fruit_profit, cosmetic_profit, baby_profit, office_profit, personal_profit,
                    beverage_profit, veggie_profit, house_profit, clothes_profit, meat_profit, cereal_profit]
     return max(the_profits)
+=======
+    product = aa
+    profit = aaa
+    item_catalog = {}
+    if product not in item_catalog:
+        item_catalog[product] = 0
+    else:
+        item_catalog[product] += float(profit)
+    return item_catalog
+
+
+def max_it_out():
+    my_items = highest_profit(the_product, the_profit)
+    the_best = max(my_items)
+    return(the_best)
+>>>>>>> parent of 9a7f041... z
 
 
 with open("Sales Records.csv", 'r') as the_Sales:
