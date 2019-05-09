2801ced8ae5b087195e2f05caaca703403a288e4
4v9z <42391837+4v9z@users.noreply.github.com>
2019-05-09 14:31:13 -0700

    i did the sales analyzer

--- notes/Armanjit Gill - Sales Analyzer.py
+++ notes/Armanjit Gill - Sales Analyzer.py
@@ -1,8 +1,10 @@
 import csv
 
 
-def highest_profit(aa, aaa):
-<<<<<<< HEAD
+with open("Sales Records.csv", 'r') as the_Sales:
+    reader = csv.reader(the_Sales)
+    print("PROCESSING THE DATA...")
+    print("FINDING HIGHEST TOTAL PROFIT...")
     fruit_profit = 0
     cosmetic_profit = 0
     baby_profit = 0
@@ -15,59 +17,55 @@ def highest_profit(aa, aaa):
     clothes_profit = 0
     meat_profit = 0
     cereal_profit = 0
-    if aa == "Fruits":
-        fruit_profit += float(aaa)
-    elif aa == "Cosmetics":
-        cosmetic_profit += float(aaa)
-    elif aa == "Baby Food":
-        baby_profit += float(aaa)
-    elif aa == "Office Supplies":
-        office_profit += float(aaa)
-    elif aa == "Personal Care":
-        personal_profit += float(aaa)
-    elif aa == "Beverages":
-        beverage_profit += float(aaa)
-    elif aa == "Vegetables":
-        veggie_profit += float(aaa)
-    elif aa == "Household":
-        house_profit += float(aaa)
-    elif aa == "Snacks":
-        snack_profit += float(aaa)
-    elif aa == "Clothes":
-        clothes_profit += float(aaa)
-    elif aa == "Meat":
-        meat_profit += float(aaa)
-    elif aa == "Cereal":
-        cereal_profit += float(aaa)
-    the_profits = [fruit_profit, cosmetic_profit, baby_profit, office_profit, personal_profit,
-                   beverage_profit, veggie_profit, house_profit, clothes_profit, meat_profit, cereal_profit]
-    return max(the_profits)
-=======
-    product = aa
-    profit = aaa
-    item_catalog = {}
-    if product not in item_catalog:
-        item_catalog[product] = 0
-    else:
-        item_catalog[product] += float(profit)
-    return item_catalog
-
-
-def max_it_out():
-    my_items = highest_profit(the_product, the_profit)
-    the_best = max(my_items)
-    return(the_best)
->>>>>>> parent of 9a7f041... z
-
-
-with open("Sales Records.csv", 'r') as the_Sales:
-    reader = csv.reader(the_Sales)
-    print("PROCESSING THE DATA...")
-    print("FINDING HIGHEST TOTAL PROFIT...")
     for row in reader:
         the_product = row[2]
         the_profit = row[13]
-        the_best_of_all = highest_profit(the_product, the_profit)
+        if the_product == "Fruits":
+            fruit_profit += float(the_profit)
+        if the_product == "Cosmetics":
+            cosmetic_profit += float(the_profit)
+        if the_product == "Baby Food":
+            baby_profit += float(the_profit)
+        if the_product == "Office Supplies":
+            office_profit += float(the_profit)
+        if the_product == "Personal Care":
+            personal_profit += float(the_profit)
+        if the_product == "Beverages":
+            beverage_profit += float(the_profit)
+        if the_product == "Vegetables":
+            veggie_profit += float(the_profit)
+        if the_product == "Household":
+            house_profit += float(the_profit)
+        if the_product == "Snacks":
+            snack_profit += float(the_profit)
+        if the_product == "Clothes":
+            clothes_profit += float(the_profit)
+        if the_product == "Meat":
+            meat_profit += float(the_profit)
+        if the_product == "Cereal":
+            cereal_profit += float(the_profit)
+        the_profits = [fruit_profit, cosmetic_profit, baby_profit, office_profit, personal_profit,
+                       beverage_profit, veggie_profit, house_profit, clothes_profit, meat_profit, cereal_profit]
 print("DONE")
-print(the_best_of_all)
-
+if max(the_profits) == fruit_profit:
+    print("The highest profit came from Fruits with a total profit of %f" % fruit_profit)
+elif max(the_profits) == cosmetic_profit:
+    print("The highest profit came from Cosmetics with a total profit of %f" % cosmetic_profit)
+elif max(the_profits) == baby_profit:
+    print("The highest profit came from Baby Food with a total profit of %f" % baby_profit)
+elif max(the_profits) == office_profit:
+    print("The highest profit came from Office Supplies with a total profit of %f" % office_profit)
+elif max(the_profits) == personal_profit:
+    print("The highest profit came from Personal Care with a total profit of %f" % personal_profit)
+elif max(the_profits) == beverage_profit:
+    print("The highest profit came from Beverages with a total profit of %f" % beverage_profit)
+elif max(the_profits) == veggie_profit:
+    print("The highest profit came from Vegetables with a total profit of %f" % veggie_profit)
+elif max(the_profits) == house_profit:
+    print("The highest profit came from Households with a total profit of %f" % house_profit)
+elif max(the_profits) == clothes_profit:
+    print("The highest profit came from Clothes with a total profit of %f" % clothes_profit)
+elif max(the_profits) == meat_profit:
+    print("The highest profit came from Meat with a total profit of %f" % meat_profit)
+elif max(the_profits) == cereal_profit:
+    print("The highest profit came from Cereal with a total profit of %f" % cereal_profit)
