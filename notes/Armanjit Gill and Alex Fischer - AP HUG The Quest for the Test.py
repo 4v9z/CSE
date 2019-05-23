842969849156d41d3b1178959441728938c2c160
blondfisch <42385460+blondfisch@users.noreply.github.com>
2019-05-23 12:06:17 -0700

    panic

--- notes/Armanjit Gill and Alex Fischer - AP HUG The Quest for the Test.py
+++ notes/Armanjit Gill and Alex Fischer - AP HUG The Quest for the Test.py
@@ -1797,8 +1797,9 @@ class Boss(Enemy):
 
 class Vonthanos(Boss):
     def __init__(self):
-        super(Vonthanos, self).__init__()
-
+        super(Vonthanos, self).__init__(Claw, 75, False, False, True, "Von Thanos", 5, 1500)
+        self.name = "Von Thanos"
+        
 
 class Bowser(Boss):
     def __init__(self):
