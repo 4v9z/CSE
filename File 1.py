0372310f48a155ac0e26e228912ab78754dc744b
4v9z <42391837+4v9z@users.noreply.github.com>
2019-08-26 15:43:10 -0700

    It's good to be back! H E L L O    W O R L D!

---
+++ File 1.py
@@ -0,0 +1,38 @@
+print("Hello Again World!")
+
+
+def function_title():
+    local_a = 7
+    local_b = 2
+    c = local_a + local_b
+    print(c)
+
+
+function_title()
+
+
+def function_two(a, b):
+    c = a*b
+    print(c)
+
+
+function_two(7, 6)
+
+
+def function_three(a, b):
+    c = a/b
+    print(c)
+
+
+Thing1 = 3
+Thing2 = 2
+function_three(3, 2)
+
+
+def function_four(a, b):
+    c = a - b
+    return c
+    # Anything underneath return will not be displayed/done
+
+
+function_four(3, 2)
