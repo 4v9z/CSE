a764e8c4ff06aa3bed1cdbf730ce43b5731badc9
Armanjit Gill <4v9z@wetigers.org>
2019-09-11 20:39:14 -0700

    Revert "ohijkugrhaik4tfreghueia"
    This reverts commit ec16f6217f740f2fdb19900356c7973d4f7880f1.

--- notes/Old Garbage/Armanjit Gill - Combo Menu, Iteration 3.py
+++ notes/Old Garbage/Armanjit Gill - Combo Menu, Iteration 3.py
@@ -12,8 +12,7 @@ def order_off_the_menu(restaurant_name):
     print(sandwich)
     if sandwich.lower() == "chicken sandwich":
         cost += 5.25
-        drin = input(colored("%s Waiter: Excellent choice, now would you like a "
-                             "drink?" % str(restaurant_name), 'green', 'on_grey'))
+        drin = input(colored("%s Waiter: Excellent choice, now would you like a drink?" % str(restaurant_name), 'green', 'on_grey'))
         if drin.lower() == 'yes':
             drink_size = input(colored("%s Waiter: Alright and what size beverage would you like?"
                                        "\n - Small: $1.00"
@@ -28,30 +27,10 @@ def order_off_the_menu(restaurant_name):
                 cost += 2.25
         elif drin.lower() == 'no':
             print(colored("%s Waiter: Ah, okay then" % str(restaurant_name), 'green', 'on_grey'))
-        frye = input(colored("%s Waiter: Would you like some fries with that?"))
-        if frye.lower() == 'yes':
-            fries = input(colored("%s Waiter: Alright then, what size fries would you like?"
-                                  "\n- Small: $1.50"
-                                  "\n- Medium: $1.50"
-                                  "\n- Large: $2.00" % str(restaurant_name), 'yellow', 'on_grey'))
-            if fries.lower() == 'small':
-                frys = input(colored("%s Waiter: Would you like to mega-size your "
-                                     "fries?" % str(restaurant_name), 'yellow', 'on_grey'))
-                if frys.lower() == 'yes':
-
-                    cost += 2.00
-                else:
-                    input(colored("%s Waiter: Fine, have it your way..."
-                                  " *under his breath* ₚₑₐₛₐₙₜ" % str(restaurant_name),
-                                  'yellow', 'on_grey'))
-                    cost += 1.00
-            elif fries.lower() == 'medium':
-                cost += 1.50
-            elif fries.lower() == 'large':
-                cost += 2.00
-            print(fries)
-        elif frye.lower() == 'no':
-            print(colored("%s Waiter: Ah, okay then" % str(restaurant_name), 'yellow', 'on_grey'))
+            if len(str(cost)) == 4:
+                print(cost)
+            else:
+                print(str(cost) + '0')
     elif sandwich.lower() == 'beef sandwich':
         cost += 6.25
         drin = input(colored("%s Waiter: Excellent choice, now would you like a drink?" % str(restaurant_name), 'green',
@@ -68,32 +47,15 @@ def order_off_the_menu(restaurant_name):
                 cost += 1.75
             elif drink_size.lower() == 'large':
                 cost += 2.25
+            frye = input(colored("%s Waiter: Would you like some fries with that?"))
+            if frye.lower() == 'yes':
+                fries = input()
         elif drin.lower() == 'no':
             print(colored("%s Waiter: Ah, okay then" % str(restaurant_name), 'green', 'on_grey'))
-        frye = input(colored("%s Waiter: Would you like some fries with that?"))
-        if frye.lower() == 'yes':
-            fries = input(colored("%s Waiter: Alright then, what size fries would you like?"
-                                  "\n- Small: $1.50"
-                                  "\n- Medium: $1.50"
-                                  "\n- Large: $2.00" % str(restaurant_name), 'yellow', 'on_grey'))
-            if fries.lower() == 'small':
-                frys = input(colored("%s Waiter: Would you like to mega-size your "
-                                     "fries?" % str(restaurant_name), 'yellow', 'on_grey'))
-                if frys.lower() == 'yes':
-
-                    cost += 2.00
-                else:
-                    input(colored("%s Waiter: Fine, have it your way..."
-                                  " *under his breath* ₚₑₐₛₐₙₜ" % str(restaurant_name),
-                                  'yellow', 'on_grey'))
-                    cost += 1.00
-            elif fries.lower() == 'medium':
-                cost += 1.50
-            elif fries.lower() == 'large':
-                cost += 2.00
-            print(fries)
-        elif frye.lower() == 'no':
-            print(colored("%s Waiter: Ah, okay then" % str(restaurant_name), 'yellow', 'on_grey'))
+        if len(str(cost)) == 4:
+            print(cost)
+        else:
+            print(str(cost) + '0')
     elif sandwich.lower() == 'tofu sandwich':
         cost += 5.75
         drin = input(colored("%s Waiter: Excellent choice, now would you like a drink?" % str(restaurant_name), 'green',
@@ -112,34 +74,10 @@ def order_off_the_menu(restaurant_name):
                 cost += 2.25
         elif drin.lower() == 'no':
             print(colored("%s Waiter: Ah, okay then" % str(restaurant_name), 'green', 'on_grey'))
-        frye = input(colored("%s Waiter: Would you like some fries with that?"))
-        if frye.lower() == 'yes':
-            fries = input(colored("%s Waiter: Alright then, what size fries would you like?"
-                                  "\n- Small: $1.50"
-                                  "\n- Medium: $1.50"
-                                  "\n- Large: $2.00" % str(restaurant_name), 'yellow', 'on_grey'))
-            if fries.lower() == 'small':
-                frys = input(colored("%s Waiter: Would you like to mega-size your "
-                                     "fries?" % str(restaurant_name), 'yellow', 'on_grey'))
-                if frys.lower() == 'yes':
-
-                    cost += 2.00
-                else:
-                    input(colored("%s Waiter: Fine, have it your way..."
-                                  " *under his breath* ₚₑₐₛₐₙₜ" % str(restaurant_name),
-                                  'yellow', 'on_grey'))
-                    cost += 1.00
-            elif fries.lower() == 'medium':
-                cost += 1.50
-            elif fries.lower() == 'large':
-                cost += 2.00
-            print(fries)
-        elif frye.lower() == 'no':
-            print(colored("%s Waiter: Ah, okay then" % str(restaurant_name), 'yellow', 'on_grey'))
-    if len(str(cost)) == 4:
-        print("Your total cost is $" + str(cost))
-    else:
-        print("Your total cost is $" + str(cost) + '0')
+        if len(str(cost)) == 4:
+            print(cost)
+        else:
+            print(str(cost) + '0')
 
 
 print(order_off_the_menu("Burger Time"))
