3e63a36069d9ffd0fb229424f51bdadb69d57133
4v9z <42391837+4v9z@users.noreply.github.com>
2019-09-26 15:42:46 -0700

    Update Armanjit Gill - Combo Menu v2 Electric Boogaloo.py

--- notes/Old Garbage/Armanjit Gill - Combo Menu v2 Electric Boogaloo.py
+++ notes/Old Garbage/Armanjit Gill - Combo Menu v2 Electric Boogaloo.py
@@ -36,7 +36,8 @@ def sandwich(a):
             print(colored("%s Computer: Alright then... I see you dislike the snadw- "
                           "I mean sanswic- I mean sandwiches. Alright then... Enjoy your next"
                           " option" % str(a)), "yellow", "on_grey")
-            break
+            aa = ""
+            return
         else:
             print(colored("%s Computer: Sorry... that isn't on the menu you nerd!" % str(a), 'green', 'on_grey'))
 
@@ -45,7 +46,7 @@ def drink(z):
     drinking = True
     order = []
     while drinking:
-        aa = input(colored("%s Waiter: Alright and what size beverage would you like?"
+        aa = input(colored("%s Computer: Alright and what size beverage would you like?"
                            "\n - Small: $1.00"
                            "\n - Medium: $1.75"
                            "\n - Large: $2.25"
@@ -72,21 +73,123 @@ def drink(z):
                           " alright... see you later alligator" % str(z)), "red", "on_grey")
             return aa
         elif aa.lower() == 'pass':
-            print(colored("%s Computer: Alright then... I see you dislike the snadw- "
-                          "I mean sanswic- I mean sandwiches. Alright then... Enjoy your next"
-                          " option" % str(z)), "green", "on_grey")
+            print(colored("%s Computer: Alright then... I see you dislike the drink selection... *sniff sniff* "
+                          "it's not like I... put a lot of time and thought into"
+                          " the options I gave you..." % str(z)), "green", "on_grey")
             break
         else:
-            print(colored("%s Computer: Sorry... that isn't on the menu you nerd!" % str(a), 'green', 'on_grey'))
+            print(colored("%s Computer: Sorry... that isn't on the menu you nerd!" % str(z), 'green', 'on_grey'))
 
+
+def fries(h):
+    drinking = True
+    order = []
+    while drinking:
+        aa = input(colored("%s Computer: Alright and what size beverage would you like?"
+                           "\n - Small: $1.00"
+                           "\n - Medium: $1.75"
+                           "\n - Large: $2.25"
+                           "\n -" % str(h), 'yellow', 'on_grey'))
+        frees = ["small", 'medium', 'large']
+        if aa.lower() in frees:
+            if aa.lower() == "small":
+                fryes = input(colored("%s Computer: Would you like to mega-size your "
+                                      "fries instead?" % str(h), "yellow", 'on_grey'))
+                if fryes.lower() in ["sure", 'yeah', 'yes', 'yee']:
+                    print(colored("%s Computer: hehehehehe sucker... - I mean alrigh"
+                                  "t... nice!" % str(h), "yellow", 'on_grey'))
+                    aa = "large"
+                elif fryes.lower() in ['no', 'nah', 'nope', 'not at all', 'no god please no. no. no. nooooooooooo']:
+                    if fryes.lower == 'no god please no. no. no. nooooooooooo':
+                        print(colored("%s Computer: Calm Down Michael Scott!" % str(h), "yellow", 'on_grey'))
+                    else:
+                        print(colored("%s Computer: Fine... have it your way..."
+                                      "ₚₑₐₛₐₙₜ" % str(h), "yellow", 'on_grey'))
+            order.append(aa)
+            b = input(colored("%s Computer: Would you like to order another set of"
+                              " fries?" % str(h), "yellow", 'on_grey'))
+            if b.lower() == "yes":
+                print(colored("%s Computer: Very well then..." % str(h), 'yellow', 'on_grey'))
+            elif b.lower() == "no":
+                print(colored("%s Computer: Okay then... good day" % str(h), 'yellow', 'on_grey'))
+                return order
+            elif b.lower() == 'pass':
+                print(colored("%s Computer: You don't like fries? Are you SALTY"
+                              " about something that happened? eh? eh?"
+                              "\n Not funny? Oh well... If you actually don't "
+                              "want fries.... why not?" % str(h)), "yellow", "on_grey")
+                break
+            else:
+                print(colored("%s Computer: I'll take that as a no... good day to you" % str(h), 'yellow', 'on_grey'))
+                return order
+        elif aa.lower() == 'q':
+            print(colored("%s Computer: Ah... I see... you would like to quit..."
+                          " alright... see you later alligator" % str(h)), "red", "on_grey")
+            return aa
+        elif aa.lower() == 'pass':
+            print(colored("%s Computer: You don't like fries? Are you SALTY"
+                          " about something that happened? eh? eh?"
+                          "\n Not funny? Oh well... If you actually don't "
+                          "want fries.... why not?" % str(h)), "yellow", "on_grey")
+            break
+        else:
+            print(colored("%s Computer: Sorry... that isn't on the menu you nerd!" % str(h), 'yellow', 'on_grey'))
+
+
+def ketchup(h):
+    ketchuping = True
+    ketches = 0
+    while ketchuping:
+        aa = input(colored("%s Computer: How many Kepchup brand ketchup packets would "
+                           "you like?" % str(h), 'red', 'on_grey'))
+        try:
+            if int(aa) == 0:
+                print(colored("%s Computer: You don't want Kepchup brand ketchup packets?"
+                              "\n You do know what Kepchup Co. does when someone doesn't want their ketchup, right?"
+                              "\n Let's just say... something red will be on their ha"
+                              "nds... and it ain't ketchup..." % str(h)), "red", "on_grey")
+                break
+            elif aa.lower() == 'q':
+                print(colored("%s Computer: Ah... I see... you would like to quit..."
+                              " alright... see you later alligator" % str(h)), "magenta", "on_grey")
+                break
+            else:
+                try:
+                    ketches += aa
+                except ValueError:
+                    print(colored("%s Computer: That isn't a number..." % str(h), "red", 'on_grey'))
+                else:
+                    b = input(colored("%s Computer: Would you like to order anymore Kepchup brand Ketchup "
+                                      "packets?" % str(h), "red", 'on_grey'))
+                    if b.lower() == "yes":
+                            print(colored("%s Computer: Very well then..." % str(h), 'red', 'on_grey'))
+                    elif b.lower() == "no":
+                        print(colored("%s Computer: Okay then... good day" % str(h), 'red', 'on_grey'))
+                        return ketches
+                    elif b.lower() == 'pass':
+                        print(colored("%s Computer: You don't want Kepchup brand ketchup packets?"
+                                      "\n You do know what Kepchup Co."
+                                      " does when someone doesn't want their ketchup, right?"
+                                      "\n Let's just say... something red will be on the"
+                                      "ir hands... and it ain't ketchup..." % str(h)), "red", "on_grey")
+                        ketchuping = False
+                        break
+                    else:
+                        print(colored("%s Computer: I'll take that as a no... good day to you" % str(h), 'red', 'on_grey'))
+                        return ketches
+        except ValueError:
+            print("That wasn't a number")
 
 def order_off_the_menu(restaurant_name):
-        if sandwich(restaurant_name) == "q":
-            return
-        drink(restaurant_name)
-        fries(restaurant_name)
-        ketchup(restaurant_name)
-        receipt(restaurant_name)
+    snadwich = sandwich(restaurant_name)
+    if snadwich == "q":
+        return
+    how_am_i_supposed_to_eat_this_pizza_without_my_drink = drink(restaurant_name)
+    if how_am_i_supposed_to_eat_this_pizza_without_my_drink == "q":
+        return
+    fries(restaurant_name)
+    ketchup(restaurant_name)
+    receipt(restaurant_name)
 
 
 order_off_the_menu("Mr. Aziz's Spider-Man 2 Pizza Time Kitchen")
