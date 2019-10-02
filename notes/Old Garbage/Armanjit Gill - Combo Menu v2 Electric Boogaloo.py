59bdcc7c66c12bfa6b1bcbed2b10952029a2e19e
4v9z <42391837+4v9z@users.noreply.github.com>
2019-10-02 15:52:15 -0700

    a

--- notes/Old Garbage/Armanjit Gill - Combo Menu v2 Electric Boogaloo.py
+++ notes/Old Garbage/Armanjit Gill - Combo Menu v2 Electric Boogaloo.py
@@ -1,4 +1,5 @@
 from termcolor import colored
+import sys
 
 
 def sandwich(a):
@@ -23,7 +24,7 @@ def sandwich(a):
             elif b.lower() == 'pass':
                 print(colored("%s Computer: Alright then... I see you dislike the snadw- "
                               "I mean sanswic- I mean sandwiches. Alright then... Enjoy your next"
-                              " option" % str(a)), "yellow", "on_grey")
+                              " option" % str(a), "yellow", "on_grey"))
                 break
             else:
                 print(colored("%s Computer: I'll take that as a no... good day to you" % str(a), 'yellow', 'on_grey'))
@@ -141,6 +142,7 @@ def fries(h):
 
 def ketchup(h):
     ketchuping = True
+    fate_of_the_world = ''
     ketches = 0
     while ketchuping:
         aa = input(colored("%s Computer: How many Kepchup brand ketchup packets would "
@@ -152,6 +154,20 @@ def ketchup(h):
                               "\n Let's just say... something red will be on their ha"
                               "nds... and it ain't ketchup..." % str(h), "red", "on_grey"))
                 break
+            elif int(aa) < 0:
+                print(colored("%s Computer: You can't have "
+                              "negative ketchup! That would destroy our very fabric "
+                              "of existence!" % str(h), "red", "on_grey"))
+                fate_of_the_world = input(colored("%s Computer: Do you still want negative"
+                                                  " ketchup? Y or N" % str(h), "red", "on_grey"))
+                if fate_of_the_world.lower() == 'y':
+                    print(colored("%s Computer: Wait! What?! NO"
+                                  "\n The fabric of our existence is tearing apart!"
+                                  "\nNOOOOOOOOOOOOOO)SU568768796Jq wgbuyrfgvbwuyrh42069jfgyuwergs7ydvbwtye6987687vftywgeytufgv4TY3GWGFYUGR"
+                                  "🕷🕸🔌🔦🔌⋷🐘🩩⋶🕚🩢𝋫𖺋∉🐘☠💀🕱⛹🙎🙋🧔😣" % str(h), "magenta", "on_grey"))
+                    sys.exit()
+                elif fate_of_the_world.lower() == 'n':
+                    print(colored("%s Computer: A wise choice..." % str(h), "magenta", "on_grey"))
             elif aa.lower() == 'q':
                 aa = 'q'
                 print(colored("%s Computer: Ah... I see... you would like to quit..."
@@ -159,7 +175,7 @@ def ketchup(h):
                 break
             else:
                 try:
-                    ketches += aa
+                    ketches += int(aa)
                 except ValueError:
                     print(colored("%s Computer: That isn't a number..." % str(h), "red", 'on_grey'))
                 else:
@@ -179,7 +195,8 @@ def ketchup(h):
                         ketchuping = False
                         break
                     else:
-                        print(colored("%s Computer: I'll take that as a no... good day to you" % str(h), 'red', 'on_grey'))
+                        print(colored("%s Computer: I'll take that as a no... good da"
+                                      "y to you" % str(h), 'red', 'on_grey'))
                         return ketches
         except ValueError:
             print("That wasn't a number")
@@ -189,14 +206,46 @@ def receipt(name, a, b, c, d):
     e = 0
     costs_snad = {'beef sandwich': 6.25,
                   'chicken sandwich': 5.25, 'tofu sandwich': 5.75}
-    for i in range(len(a)):
-        if a[i] in costs_snad:
-            e += costs_snad[a[i]]
-
+    costs_dronk = {'small': 1.00,
+                   'medium': 1.75, 'large': 2.25}
+    costs_frye = {'small': 1.00,
+                  'medium': 1.50, 'large': 2.00}
+    if a is None:
+        print(colored("You didn't order any sandwiches", 'white', 'on_grey'))
+    else:
+        print(colored("You ordered the following sandwiches:", 'white', 'on_grey'))
+        for i in range(len(a)):
+            if a[i] in costs_snad:
+                e += costs_snad[a[i]]
+                print(str(i + 1) + ": " + colored(a[i], 'white', 'on_grey') + colored(' - ', 'white', 'on_grey') +
+                      colored(costs_snad[a[i]], 'white', 'on_grey'))
+    if b is None:
+        print(colored("You didn't order any drinks", 'white', 'on_grey'))
+    else:
+        print(colored("You ordered the following drinks:", 'white', 'on_grey'))
+        for i in range(len(b)):
+            if a[i] in costs_dronk:
+                e += costs_dronk[a[i]]
+                print(str(i + 1) + ": a" + colored(a[i], 'white', 'on_grey') + colored(' drink - ', 'white',
+                                                                                       'on_grey') + colored(
+                    costs_dronk[a[i]], 'white', 'on_grey'))
+    if c is None:
+        print(colored("You didn't order any fries", 'white', 'on_grey'))
+    else:
+        print(colored("You ordered the following fries:", 'white', 'on_grey'))
+        for i in range(len(c)):
+            if a[i] in costs_frye:
+                e += costs_frye[a[i]]
+                print(str(i + 1) + ": " + colored(a[i], 'white', 'on_grey') + colored(' fries - ', 'white',
+                                                                                      'on_grey') + colored(
+                    costs_frye[a[i]], 'white', 'on_grey'))
+    print(colored("You also ordered %d Kepchup brand Ketchup packets which "
+                  "costed $%f" % (d, .25 * d), 'white', 'on_grey'))
+    e += .25 * d
     e += e * 0.07
     if e >= 15.00:
         e -= 5.00
-    if "chicken sandwich" in a and "small" in b and "small" in c:
+    if "chicken sandwich" in a and len(a) == 1 and "small" in b and len(b) == 1 and "small" in c and len(c) == 1:
         e -= 2.50
     if len(a) > 0 and b is None and c is None and d is None:
         e -= 1.25
@@ -214,6 +263,13 @@ def receipt(name, a, b, c, d):
     if str(e) == "0.0":
         e = "0.00"
     print("Your total comes out to $" + str(e))
+    again = input(colored("%s Computer: Would you like to order another "
+                          "meal? Y or N" % str(name), "magenta", "on_grey"))
+    if again.lower() == 'y':
+        order_off_the_menu(name)
+    else:
+        print(colored("%s Computer: I'll take that as a no" % str(name), "magenta", "on_grey"))
+        sys.exit()
 
 
 def order_off_the_menu(restaurant_name):
@@ -226,10 +282,12 @@ def order_off_the_menu(restaurant_name):
     else:
         print(colored("I did not get a proper response, so I'll take that as a yes!", 'red', 'on_grey'))
         print(colored("%s Computer: Hewwo UwU, and welcome to %s owo! If at any point you would like to quit, simply "
-                      "type in 'q', but don't think that means you'll escape the owo army! \n Should you want to pass on a menu item, type in 'pass'."
+                      "type in 'q', but don't think that means you'll escape the owo "
+                      "army! \n Should you want to pass on a menu item, type in 'pass'."
                       " However, we'll also make sure you pass too!. However, "
                       "if you don't want ketchup, don't type in 'pass', instead, type in '0', not 0w0, when asked "
-                      "how many packets you'd like owo uwu IuI" % (restaurant_name, restaurant_name), 'magenta', 'on_grey'))
+                      "how many packets you'd like owo uwu "
+                      "IuI" % (restaurant_name, restaurant_name), 'magenta', 'on_grey'))
     snadwich = sandwich(restaurant_name)
     if snadwich == "q":
         return
