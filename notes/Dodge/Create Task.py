6ce7439e8488173dd247b3bdc68c6f3bcd6bda38
4v9z <42391837+4v9z@users.noreply.github.com>
2019-11-14 16:17:07 -0800

    sper mario and suff

--- notes/Dodge/Create Task.py
+++ notes/Dodge/Create Task.py
@@ -11,6 +11,7 @@ s1 = True
 s2 = False
 gaming = True
 screen = pygame.display.set_mode([550,400])
+Mouse = [0]*2
 title = pygame.image.load("title.png").convert()
 menu = pygame.image.load("menu.png").convert()
 crctr_select = pygame.image.load("character1.png").convert()
@@ -19,6 +20,9 @@ game_clock = pygame.time.Clock()
 animationclock = pygame.time.Clock()
 pygame.display.set_caption("SUPER SMASH BROS. PYTHON EDITION")
 
+black = (0, 0, 0)
+white = (255, 255, 255)
+generic_the_color = (42,0,69)
 
 def update_menu(b):
     if b:
@@ -37,7 +41,44 @@ def player(a, b):
     elif b == "stop":
         a.stop()
 
+
+class cursor1(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        cursorimg = pygame.image.load("cursor.png")
+        self.image = pygame.Surface([32,32])
+        self.image.set_colorkey(black)
+        self.image.blit(cursorimg, (0,0))
+        self.rect = self.image.get_rect()
+        self.rect.x = 350
+        self.rect.y = 200
+
+    def Move(self, Mouse):
+        self.rect.x = Mouse[0]
+        self.rect.y = Mouse[1]
+
+
+class ctrport(pygame.sprite.Sprite):
+    def __init__(self, image, x, y, crctr):
+        pygame.sprite.Sprite.__init__(self)
+        portimg = pygame.image.load(image)
+        self.image = pygame.Surface([32, 32])
+        self.image.set_colorkey(generic_the_color)
+        self.image.blit(portimg, (0,0))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.character = crctr
+
+
+p1cursor = cursor1()
+supermario = ctrport('mario crctr port.png', 70, 60, 'mario')
+characterportraits = pygame.sprite.Group()
+characterportraits.add(supermario)
+pygame.mouse.set_visible(False)
 playing = False
+cursors = pygame.sprite.Group()
+cursors.add(p1cursor)
 while starting:
     playing = player(titletheme, playing)
     for event in pygame.event.get():
@@ -58,6 +99,9 @@ while menuing:
         if event.type == pygame.QUIT:
             menuing = False
             sys.exit()
+        if event.type == pygame.MOUSEMOTION:
+            Mouse = list(event.pos)
+            p1cursor.Move(Mouse)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
                 if not ctr:
@@ -66,6 +110,17 @@ while menuing:
                 if s1:
                     ctr = True
                     screen.blit(crctr_select, [0,0])
+    if ctr:
+        screen.blit(crctr_select, [0, 0])
+    else:
+        if s1:
+            screen.blit(menu, [0,0])
+        if not s1 or s2:
+            screen.blit(menu2, [0,0])
+    if ctr:
+        characterportraits.draw(screen)
+        cursors.draw(screen)
+
 
     if not ctr:
         if s1:
