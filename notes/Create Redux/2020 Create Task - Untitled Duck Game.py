0a8f8325624a03ad6d0a2cb4e528b8521d948ee2
Armanjit Gill <4v9z@wetigers.org>
2020-04-26 19:02:54 -0700

    SONIC DUCK      BOTTOM TEXT

--- notes/Create Redux/2020 Create Task - Untitled Duck Game.py
+++ notes/Create Redux/2020 Create Task - Untitled Duck Game.py
@@ -3,8 +3,8 @@ pygame.init()
 black = (0, 0, 0)
 blackish = (1, 1, 1)
 white = (255, 255, 255)
-generic_color = (42,0,69)
-screen = pygame.display.set_mode([580,500])
+generic_color = (42, 0, 69)
+screen = pygame.display.set_mode([580, 500])
 pygame.display.set_caption("Untitled Duck Game")
 pygame.mouse.set_visible(False)
 title_screen = pygame.image.load("duck title.png").convert()
@@ -12,21 +12,48 @@ basic_sky = pygame.image.load("environments.png").convert()
 
 titling = True
 FpS = pygame.time.Clock()
+PowerUps = pygame.sprite.Group()
 
 
-class ground(pygame.sprite.Sprite):
+class Ground(pygame.sprite.Sprite):
     def __init__(self, imge, x, y):
         pygame.sprite.Sprite.__init__(self)
         img = pygame.image.load(imge).convert()
         self.image = pygame.Surface([580, 30])
         self.image.set_colorkey(black)
-        self.image.blit(img, (0,0))
+        self.image.blit(img, (0, 0))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
 
-grass_ground = ground("ground.png", 0, 470)
+grass_ground = Ground("ground.png", 0, 470)
+
+
+class power_up(pygame.sprite.Sprite):
+    def __init__(self, imge, x, y, name):
+        pygame.sprite.Sprite.__init__(self)
+        self.img = pygame.image.load(imge).convert()
+        self.image = pygame.Surface([32, 32])
+        self.image.set_colorkey(black)
+        self.image.blit(self.img, (0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.name = name
+        self.touchin_player = 0
+        self.collectedd = False
+
+    def collected(self):
+        self.touchin_player = pygame.sprite.groupcollide(DuckSprites, PowerUps, False, False)
+        if len(self.touchin_player) > 0:
+            if self.name == "Cheetah Orb":
+                if not self.collectedd:
+                    print("Congrats! Now you can become a Cheetah Duck!"
+                          "\nThis lets you run faster at the cost of a reduced jump height")
+                    self.collectedd = True
+            else:
+                print("Wat?")
 
 
 class Duck(pygame.sprite.Sprite):
@@ -35,10 +62,12 @@ class Duck(pygame.sprite.Sprite):
         self.image = pygame.Surface([40, 52])
         self.image.set_colorkey(black)
         self.rect = self.image.get_rect()
+        self.type = "d"
         self.rect.x = 310
+        self.jumps = 1
         self.rect.y = 420
         self.touchin_ground = 0
-
+        self.can_trans = False
         self.image.blit(pygame.image.load("Duck.png"), (0, 0))
 
     def move(self, shmovement):
@@ -50,20 +79,41 @@ class Duck(pygame.sprite.Sprite):
             self.rect.x = 5
 
     def jump(self):
-        if 495 >= self.rect.y >= 5:
-            self.rect.y -= 80
-        if self.rect.y < 5:
-            self.rect.y = 420
-        if self.rect.y > 495:
-            self.rect.y = 420
+        if self.jumps == 1:
+            self.jumps = 0
+            if 495 >= self.rect.y >= 5:
+                self.rect.y -= 80
+            if self.rect.y < 5:
+                self.rect.y = 420
+            if self.rect.y > 495:
+                self.rect.y = 420
 
     def gravity(self):
         self.touchin_ground = pygame.sprite.groupcollide(DuckSprites, Enviros, False, False)
         if len(self.touchin_ground) > 0:
             grav = 0
+            self.jumps = 1
         else:
             grav = 3
         self.rect.y += grav
+
+    def transform(self, plan):
+
+        if self.can_trans:
+            if self.type == plan:
+                print("I'm already what you need me to be")
+            if plan == "c":
+                self.image = pygame.Surface([40, 52])
+                self.image.blit(pygame.image.load("CDuck.png"), (0, 0))
+                self.image.set_colorkey(black)
+                self.type = "c"
+            elif plan == "d":
+                self.image = pygame.Surface([40, 52])
+                self.image.blit(pygame.image.load("Duck.png"), (0, 0))
+                self.image.set_colorkey(black)
+                self.type = "d"
+        else:
+            print("You fool, you can't achieve metamorphosis yet")
 
 
 The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard = Duck()
@@ -71,9 +121,12 @@ x = 0
 y = 0
 DuckSprites = pygame.sprite.Group()
 DuckSprites.add(The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard)
-Enviro = pygame.sprite.Group()
-Enviro.add(grass_ground)
+Enviro1 = pygame.sprite.Group()
+Enviro1.add(grass_ground)
 Enviros = [grass_ground]
+
+The_Faster_Mallard = power_up("CheetahPowUp.png", 420, 420, "Cheetah Orb")
+PowerUps.add(The_Faster_Mallard)
 gaming = True
 while titling:
     for event in pygame.event.get():
@@ -96,13 +149,21 @@ while gaming:
                 x = 5
             if event.key == pygame.K_UP:
                 The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.jump()
+            if event.key == pygame.K_c:
+                The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.transform("c")
+            if event.key == pygame.K_d:
+                The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.transform("d")
         if event.type == pygame.KEYUP:
             x = 0
             y = 0
     screen.blit(basic_sky, [0, 0])
     The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.move(x)
     The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.gravity()
+    PowerUps.draw(screen)
+    Enviro1.draw(screen)
     DuckSprites.draw(screen)
-    Enviro.draw(screen)
     pygame.display.flip()
+    The_Faster_Mallard.collected()
+    if The_Faster_Mallard.collectedd:
+        The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.can_trans = True
     FpS.tick(16)
