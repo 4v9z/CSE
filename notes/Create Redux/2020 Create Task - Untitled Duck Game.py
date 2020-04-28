ff18d02de2101694144504ca0bc9ba4fa79c3201
Armanjit Gill <4v9z@wetigers.org>
2020-04-27 18:32:26 -0700

    progress

--- notes/Create Redux/2020 Create Task - Untitled Duck Game.py
+++ notes/Create Redux/2020 Create Task - Untitled Duck Game.py
@@ -9,7 +9,8 @@ pygame.display.set_caption("Untitled Duck Game")
 pygame.mouse.set_visible(False)
 title_screen = pygame.image.load("duck title.png").convert()
 basic_sky = pygame.image.load("environments.png").convert()
-
+icon1 = pygame.image.load("Ducon.png")
+icon2 = pygame.image.load("CDuckon.png")
 titling = True
 FpS = pygame.time.Clock()
 PowerUps = pygame.sprite.Group()
@@ -22,12 +23,33 @@ class Ground(pygame.sprite.Sprite):
         self.image = pygame.Surface([580, 30])
         self.image.set_colorkey(black)
         self.image.blit(img, (0, 0))
+        self.type = "ground"
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+class Plat(pygame.sprite.Sprite):
+    def __init__(self, imge, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load(imge).convert()
+        self.image = pygame.Surface([80, 20])
+        self.image.set_colorkey(black)
+        self.type = "platform"
+        self.image.blit(img, (0, 0))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
 
 grass_ground = Ground("ground.png", 0, 470)
+grass_platform1 = Plat("gplatform.png", 500, 405)
+grass_platform2 = Plat("gplatform.png", 360, 390)
+grass_platform3 = Plat("gplatform.png", 220, 375)
+grass_platform4 = Plat("gplatform.png", 220, 330)
+grass_platform5 = Plat("gplatform.png", 220, 285)
+grass_platform6 = Plat("gplatform.png", 360, 270)
+grass_platform7 = Plat("gplatform.png", 500, 255)
 
 
 class power_up(pygame.sprite.Sprite):
@@ -65,32 +87,76 @@ class Duck(pygame.sprite.Sprite):
         self.type = "d"
         self.rect.x = 310
         self.jumps = 1
+        self.direction = 1
         self.rect.y = 420
         self.touchin_ground = 0
         self.can_trans = False
+        self.runnin = False
         self.image.blit(pygame.image.load("Duck.png"), (0, 0))
 
     def move(self, shmovement):
-        if 575 >= self.rect.x >= 5:
-            self.rect.x = self.rect.x + shmovement
-        if self.rect.x < 5:
-            self.rect.x = 5
-        if self.rect.x > 575:
-            self.rect.x = 5
+        if self.type == "d":
+            if 575 >= self.rect.x >= 5:
+                self.rect.x = self.rect.x + shmovement
+            if self.rect.x < 5:
+                self.rect.x = 5
+            if self.rect.x > 575:
+                self.rect.x = 5
+            if self.direction == 1:
+                self.image = pygame.Surface([40, 52])
+                self.image.blit(pygame.image.load("Duck.png"), (0, 0))
+                self.image.set_colorkey(black)
+            else:
+                self.image = pygame.Surface([40, 52])
+                self.image.blit(pygame.image.load("DuckL.png"), (0, 0))
+                self.image.set_colorkey(black)
+        elif self.type == "c":
+            self.image = pygame.Surface([40, 52])
+            if self.runnin:
+                if self.direction == 1:
+                    self.image.blit(pygame.image.load("CDuckRun.png"), (0, 0))
+                else:
+                    self.image = pygame.Surface([40, 52])
+                    self.image.blit(pygame.image.load("CDuckRunL.png"), (0, 0))
+                    self.image.set_colorkey(black)
+            else:
+                if self.direction == 1:
+                    self.image.blit(pygame.image.load("CDuck.png"), (0, 0))
+                else:
+                    self.image = pygame.Surface([40, 52])
+                    self.image.blit(pygame.image.load("CDuckL.png"), (0, 0))
+                    self.image.set_colorkey(black)
+            self.image.set_colorkey(black)
+            if 575 >= self.rect.x >= 5:
+                self.rect.x = self.rect.x + (shmovement*4)
+            if self.rect.x < 5:
+                self.rect.x = 5
+            if self.rect.x > 575:
+                self.rect.x = 5
 
     def jump(self):
         if self.jumps == 1:
             self.jumps = 0
-            if 495 >= self.rect.y >= 5:
-                self.rect.y -= 80
-            if self.rect.y < 5:
-                self.rect.y = 420
-            if self.rect.y > 495:
-                self.rect.y = 420
+            if self.type == "d":
+                if 495 >= self.rect.y >= 5:
+                    self.rect.y -= 80
+                if self.rect.y < 5:
+                    self.rect.y = 420
+                if self.rect.y > 495:
+                    self.rect.y = 420
+            elif self.type == "c":
+                if 495 >= self.rect.y >= 5:
+                    self.rect.y -= 60
+                if self.rect.y < 5:
+                    self.rect.y = 420
+                if self.rect.y > 495:
+                    self.rect.y = 420
 
     def gravity(self):
         self.touchin_ground = pygame.sprite.groupcollide(DuckSprites, Enviros, False, False)
+
         if len(self.touchin_ground) > 0:
+            if self.touchin_ground[0]
             grav = 0
             self.jumps = 1
         else:
@@ -123,9 +189,17 @@ DuckSprites = pygame.sprite.Group()
 DuckSprites.add(The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard)
 Enviro1 = pygame.sprite.Group()
 Enviro1.add(grass_ground)
-Enviros = [grass_ground]
+Enviro1.add(grass_platform1)
+Enviro1.add(grass_platform2)
+Enviro1.add(grass_platform3)
+Enviro1.add(grass_platform4)
+Enviro1.add(grass_platform5)
+Enviro1.add(grass_platform6)
+Enviro1.add(grass_platform7)
+Enviros = [grass_ground, grass_platform1, grass_platform2, grass_platform3,
+           grass_platform4, grass_platform5, grass_platform6, grass_platform7]
 
-The_Faster_Mallard = power_up("CheetahPowUp.png", 420, 420, "Cheetah Orb")
+The_Faster_Mallard = power_up("CheetahPowUp.png", 540, 180, "Cheetah Orb")
 PowerUps.add(The_Faster_Mallard)
 gaming = True
 while titling:
@@ -144,8 +218,14 @@ while gaming:
             gaming = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
+                The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.direction = 0
+                if The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.type == "c":
+                    The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.runnin = True
                 x = -5
             if event.key == pygame.K_RIGHT:
+                The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.direction = 1
+                if The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.type == "c":
+                    The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.runnin = True
                 x = 5
             if event.key == pygame.K_UP:
                 The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.jump()
@@ -156,14 +236,24 @@ while gaming:
         if event.type == pygame.KEYUP:
             x = 0
             y = 0
+            The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.runnin = False
     screen.blit(basic_sky, [0, 0])
     The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.move(x)
     The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.gravity()
     PowerUps.draw(screen)
     Enviro1.draw(screen)
     DuckSprites.draw(screen)
-    pygame.display.flip()
+    if not The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.runnin:
+        if The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.type == "c":
+            The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.image = pygame.Surface([40, 52])
+            The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.image.blit(pygame.image.load("CDuck.png"), (0, 0))
+            The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.image.set_colorkey(black)
     The_Faster_Mallard.collected()
     if The_Faster_Mallard.collectedd:
         The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.can_trans = True
+        screen.blit(icon1, [20, 20])
+        screen.blit(icon2, [20, 60])
+    pygame.display.flip()
+    print(The_Man_With_A_Plan_The_Mallard_Thats_A_Hazard.rect.y)
+    print(grass_platform1.rect.y)
     FpS.tick(16)
